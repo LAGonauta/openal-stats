732e2b001aab2a34c1cd8b320fd365104ad11822
LAGonauta <lagonauta@gmail.com>
2025-03-04 06:05:12 -0500

    Initial code with Slint UI

--- openal_stats_viewer/src/main.rs
+++ openal_stats_viewer/src/main.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashMap, io};
+slint::include_modules!();
+
+use std::{collections::HashMap, io, thread};
 
 use anyhow::Context;
 use interprocess::local_socket::{tokio::Stream, traits::tokio::Listener, GenericNamespaced, ListenerOptions, ToNsName};
@@ -9,49 +11,63 @@ use tokio_util::sync::CancellationToken;
 // TODO: add proper coloured logging
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
-    // create RT by hand, in its own thread.
-    // the main thread will be owned by Slint.
     let tk = CancellationToken::new();
-    tokio::spawn(async move {
-        tk.run_until_cancelled(async {
-            let socket_name = "openal_stats.sock";
-            let name = socket_name.to_ns_name::<GenericNamespaced>()?;
+    let server_runtime_thread = thread::spawn({
+        let tk = tk.clone();
+        || {
+            let rt = tokio::runtime::Builder::new_current_thread().enable_all().build()?;
+            rt.block_on(async move {
+                tk.run_until_cancelled(async move {
+                    let socket_name = "openal_stats.sock";
+                    let name = socket_name.to_ns_name::<GenericNamespaced>()?;
+        
+                    let opts = ListenerOptions::new()
+                        .name(name);
+        
+                    let listener = match opts.create_tokio() {
+                        Ok(l) => l,
+                        Err(e) => todo!("{}", e),
+                    };
+        
+                    eprintln!("Server running at {socket_name}");
+        
+                    loop {
+                        // Sort out situations when establishing an incoming connection caused an error.
+                        let conn = match listener.accept().await {
+                            Ok(c) => c,
+                            Err(e) => {
+                                eprintln!("There was an error with an incoming connection: {e}");
+                                continue;
+                            }
+                        };
+        
+                        // Spawn new parallel asynchronous tasks onto the Tokio runtime and hand the connection
+                        // over to them so that multiple clients could be processed simultaneously in a
+                        // lightweight fashion.
+                        tokio::spawn(async move {
+                            // The outer match processes errors that happen when we're connecting to something.
+                            // The inner if-let processes errors that happen during the connection.
+                            if let Err(e) = handle_conn(conn).await {
+                                eprintln!("Error while handling connection: {e}");
+                            }
+                        });
 
-            let opts = ListenerOptions::new()
-                .name(name);
-
-            let listener = match opts.create_tokio() {
-                Ok(l) => l,
-                Err(e) => todo!("{}", e),
-            };
-
-            eprintln!("Server running at {socket_name}");
-
-            loop {
-                // Sort out situations when establishing an incoming connection caused an error.
-                let conn = match listener.accept().await {
-                    Ok(c) => c,
-                    Err(e) => {
-                        eprintln!("There was an error with an incoming connection: {e}");
-                        continue;
+                        if false {
+                            break;
+                        }
                     }
-                };
+        
+                    anyhow::Ok(())
+                }).await.context("cancelled")
+            })
+        }
+    });
 
-                // Spawn new parallel asynchronous tasks onto the Tokio runtime and hand the connection
-                // over to them so that multiple clients could be processed simultaneously in a
-                // lightweight fashion.
-                tokio::spawn(async move {
-                    // The outer match processes errors that happen when we're connecting to something.
-                    // The inner if-let processes errors that happen during the connection.
-                    if let Err(e) = handle_conn(conn).await {
-                        eprintln!("Error while handling connection: {e}");
-                    }
-                });
-            }
+    let ui = AppWindow::new()?;
+    ui.run()?;
 
-            anyhow::Ok(())
-        }).await.context("cancelled")
-    }).await???;
+    tk.cancel();
+    _ = server_runtime_thread.join();
 
     Ok(())
 }
